7df464c3992de707537fd0a2e6f7af0e504476a0
BuildTools <unconfigured@null.spigotmc.org>
2024-06-05 18:42:42 +0200

    commit

---
+++ src/components/nav.js
@@ -0,0 +1,9 @@
+// nav.js
+document.getElementById('navbar').innerHTML = `
+    <ul>
+        <li><a href="index.html">Accueil</a></li>
+        <li><a href="actualites.html">Actualités</a></li>
+        <li><a href="projet.html">Projet</a></li>
+        <li><a href="contact.html">Contact</a></li>
+    </ul>
+`;
