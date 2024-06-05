36eda72ac725b833168b604abc672c55f5e7b1ca
BuildTools <unconfigured@null.spigotmc.org>
2024-06-05 18:57:27 +0200

    commit

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,18 @@
 // nav.js
-document.getElementById('navbar').innerHTML = `
-    <ul>
-        <li><a href="index.html">Accueil</a></li>
-        <li><a href="actualites.html">Actualités</a></li>
-        <li><a href="projet.html">Projet</a></li>
-        <li><a href="contact.html">Contact</a></li>
-    </ul>
-`;
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navbarContainer = document.getElementById('navbar-container');
+
+    const navbarHTML = `
+        <nav class="navbar">
+            <ul class="navbar-list">
+                <li class="navbar-item"><a href="index.html">Accueil</a></li>
+                <li class="navbar-item"><a href="actualites.html">Actualités</a></li>
+                <li class="navbar-item"><a href="contact.html">Contact</a></li>
+                <li class="navbar-item"><a href="about.html">À propos</a></li>
+            </ul>
+        </nav>
+    `;
+
+    navbarContainer.innerHTML = navbarHTML;
+});
